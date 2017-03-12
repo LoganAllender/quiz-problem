b1968fa25e6711110a07faaed797d423550a81d7
LoganAllender <lallender11@gmail.com>
2017-03-12 12:20:52 -0500

    Update quiz1.js

--- js/quiz1.js
+++ js/quiz1.js
@@ -1,53 +1,51 @@
-// use to print message
-function print(message) {
-  document.write(message);
-}
-
-
-// Ask a question
-function askQuestion(question){
-  answer = prompt(question);
-  return answer.toLowerCase;
-} 
-
-//  HTML Setup for Printing Results
-
-var correctHTML = 'You got these question(s) correct: <ol>';
-var wrongHTML =  'You got these question(s) wrong: <ol>'
-  
-
-// Arrays of Questions and Answers
-
 var Quiz = [
-  ['What is the Capitol of Texas?' , 'Austin'],
-  ['How many states are in the USA?' , 50],
-  ['Is Javascript getting harder' , 'Yes'],  
+  ['How many states are in the USA?' , 50], 
+  ['How many continents are there?' , 7],
+  ['How many legs does a spider have?' , 8],
 ];
-  
-  
-// Loop of Questions
-  
-  var correctAnswer = 0
-  var quizQuestion = Quiz[i][0];
-  var quizAnswer = Quiz[i][1];
 
-   
-  for ( var i = 0 ; i < Quiz.length ; i += 1 ) {
-    askQuestion(quizQuestion);
-    
-} if ( answer === quizAnswer) {
-  correctAnswer += 1;
-  correctHTML += '<li>' + quizQuestion + '</li>'
-} else {
-  wrongHTML += '<li>' + quizQuestion+ '</li>'
+var correctAnswers = 0 ;
+var question;
+var answer;
+var response;
+var html;
+var correctAnswersArray = []; // Collect correct answers to new array 
+var wrongAnswersArray = []; //  College wrong answers to new array 
+
+function print(message){
+ var outputDiv = document.getElementById('output');
+  outputDiv.innerHTML = message;
+}
+
+function buildList(arr) {
+  var listHTML = '<ol>';
+  for ( var i = 0 ; i < arr.length ; i += 1 ){
+    listHTML += '<li>' + arr[i] + '</li>' ;
+  }
+  listHTML += '</ol>';
+  return listHTML; 
 }
 
 
-// Print the Final HTML Results
+for ( var i = 0 ; i < Quiz.length ; i += 1 ){
+     question = Quiz[i][0];
+     answer = Quiz[i][1];
+     response = prompt(question);
+     response = parseInt(response);
+} if ( response === answer ) {
+  correctAnswers += 1;
+  correctAnswersArray.push(question);
+} else {
+  wrongAnswersArray.push(question);
+}
 
-correctHTML += '</ol>';
-wrongHTML += '</ol>';
 
-print(correctHTML);
-print(wrongHTML);
+html = 'You got ' + correctAnswers + ' correct <br>';
+html += 'You got these questions correct:';
+html += buildList(correctAnswersArray);
+html += 'You got these questions wrong:';
+html += buildList(wrongAnswersArray);
+print(html);
+
+
   
